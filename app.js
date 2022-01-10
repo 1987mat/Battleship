3915f0508c6876d87e4911ae0ed9f00f778afe88
matcalvani <matstraty@gmail.com>
2022-01-09 17:23:16 -0800

    update

--- app.js
+++ app.js
@@ -10,51 +10,59 @@
 
 // CREATE BOARDS ON UI
 const userBoard = document.querySelector('.your-board');
-const compBoard = document.querySelector('.enemy-board');
+// const compBoard = document.querySelector('.enemy-board');
 
 const arrPlayer = [];
-const arrComp = [];
+const ship1 = [];
 
-const width = 10;
-
-// Create board function
 function createBoard(board, array) {
-  for (let i = 0; i < width * width; i++) {
+  for (let i = 0; i < 100; i++) {
     const square = document.createElement('div');
-    square.dataset.id = i;
+    square.id = i;
+    square.innerHTML = 'O';
     square.classList.add('square');
-    array.push(square);
+    array.push(square.innerHTML);
+    board.appendChild(square);
+  }
+}
+
+function createShip(array, length) {
+  for (let i = 0; i < length; i++) {
+    const shipSquare = document.createElement('div');
+    shipSquare.id = i;
+    shipSquare.innerHTML = 'X';
+    array.push(shipSquare.innerHTML);
+  }
+}
+
+function placeShip(array, shipArr) {
+  const randomPosition = () => Math.floor(Math.random() * (99 - 0 + 1) + 0);
+  array.splice(randomPosition(), shipArr.length, ...shipArr);
+  return array;
+}
+
+function updateBoard(array, board) {
+  array.forEach((item) => {
+    if (item !== 'X') {
+      item = 'O';
+    }
+  });
+  console.log(array);
+  renderBoard(array, board);
+}
+
+function renderBoard(array, board) {
+  board.innerHTML = '';
+  for (let i = 0; i < array.length; i++) {
+    const square = document.createElement('div');
+    square.id = i;
+    square.innerHTML = array[i];
+    square.classList.add('square');
     board.appendChild(square);
   }
 }
 
 createBoard(userBoard, arrPlayer);
-createBoard(compBoard, arrComp);
-
-// DRAG AND DROP SHIPS
-const isOccupied = false;
-
-let number;
-
-document.getElementById('drag-div').ondragstart = (e) => {
-  document.getElementById('tracker').innerText = e.target.childElementCount;
-  number = e.target.childElementCount;
-};
-
-document.getElementById('drag-div').ondragend = (e) => {
-  document.getElementById('tracker').innerText = 'end';
-};
-
-userBoard.ondragover = (e) => {
-  e.preventDefault();
-};
-
-userBoard.ondrop = (e) => {
-  const dropzone = e.target;
-
-  dropzone.style.background = 'lightblue';
-  dropzone.classList.add('occupied');
-  console.log(dropzone);
-};
-
-// drag ship div onto the board and place it / set isOccupied to true
+createShip(ship1, 4);
+placeShip(arrPlayer, ship1);
+updateBoard(arrPlayer, userBoard);
