f11b1d2cac370b3b34e67a5055d9baca43869fae
matcalvani <matstraty@gmail.com>
2022-01-18 19:26:45 -0800

    update

--- app.js
+++ app.js
@@ -1,29 +1,78 @@
-// CREATE BOARDS ON UI
+/* - Create ships
+
+   - Place player's ships
+   - Mark the spots as taken
+   - Randomly place ships for Computer Board
+   - Start the game 
+*/
 const userBoard = document.querySelector('.player-board');
-// const compBoard = document.querySelector('.computer-board');
+const compBoard = document.querySelector('.computer-board');
+const shipsDiv = document.querySelectorAll('.ship-wrapper .boat');
 
-const arrPlayer = [];
-const ship1 = [];
+const playerArr = [];
+const compArr = [];
 
-function createBoard(board, array) {
+const destroyerArr = [1, 2, 3, 4, 5];
+const cruiserArr = [1, 2, 3, 4];
+const battleshipArr = [1, 2, 3];
+const submarineArr = [1, 2];
+const carrierArr = [1, 2];
+
+createBoard(userBoard, playerArr);
+createBoard(compBoard, compArr);
+rotatePlayerShips(shipsDiv);
+
+/* DRAG AND DROP SHIPS */
+
+// Loop through the ships and attach dragstart event
+document.querySelectorAll('.drag').forEach((item) => {
+  item.addEventListener('dragstart', (e) => {
+    document.querySelector('.status').innerHTML = `${e.target}`;
+    let el = e.target.firstChild.nextElementSibling;
+    let arr = Array.from(el.children);
+    let squareArr = arr.map((val) => {
+      return val.id;
+    });
+  });
+});
+
+// Fire dragover and drop event on userBoard
+userBoard.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+userBoard.addEventListener('drop', (e) => {
+  e.preventDefault();
+
+  let arr = Array.from(e.target.parentElement.children);
+});
+
+function createBoard(board, arr) {
   for (let i = 0; i < 100; i++) {
     const square = document.createElement('div');
     square.id = i;
     square.innerHTML = '';
     square.classList.add('square');
-    array.push(square.innerHTML);
+    arr.push(square.innerHTML);
     board.appendChild(square);
   }
 }
 
-// function createShip(array, length) {
-//   for (let i = 0; i < length; i++) {
-//     const shipSquare = document.createElement('div');
-//     shipSquare.id = i;
-//     shipSquare.innerHTML = 'X';
-//     array.push(shipSquare.innerHTML);
-//   }
-// }
+function rotatePlayerShips() {
+  shipsDiv.forEach((item) => {
+    item.addEventListener('dblclick', () => {
+      if (!item.classList.contains('horizontal')) {
+        item.classList.remove('vertical');
+        item.classList.add('horizontal');
+        item.style.transform = 'rotate(90deg)';
+      } else {
+        item.classList.remove('horizontal');
+        item.classList.add('vertical');
+        item.style.transform = 'none';
+      }
+    });
+  });
+}
 
 // function placeShip(array, shipArr) {
 //   const randomPosition = () => Math.floor(Math.random() * (99 - 0 + 1) + 0);
@@ -52,7 +101,5 @@ function createBoard(board, array) {
 //   }
 // }
 
-createBoard(userBoard, arrPlayer);
-// createShip(ship1, 4);
 // placeShip(arrPlayer, ship1);
 // updateBoard(arrPlayer, userBoard);
