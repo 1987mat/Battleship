bd992e7e2408032d2f24ac49b744bc3bf8132e10
matcalvani <matstraty@gmail.com>
2022-01-21 15:33:28 -0800

    place ship

--- app.js
+++ app.js
@@ -12,18 +12,11 @@ const shipsDiv = document.querySelectorAll('.ship-wrapper .boat');
 const playerArr = [];
 const compArr = [];
 
-const destroyerArr = [1, 2, 3, 4, 5];
-const cruiserArr = [1, 2, 3, 4];
-const battleshipArr = [1, 2, 3];
-const submarineArr = [1, 2];
-const carrierArr = [1, 2];
-
 createBoard(userBoard, playerArr);
-createBoard(compBoard, compArr);
+// createBoard(compBoard, compArr);
 rotatePlayerShips(shipsDiv);
 
 /* DRAG AND DROP SHIPS */
-
 // Loop through the ships and attach dragstart event
 document.querySelectorAll('.drag').forEach((item) => {
   item.addEventListener('dragstart', (e) => {
@@ -43,15 +36,14 @@ userBoard.addEventListener('dragover', (e) => {
 
 userBoard.addEventListener('drop', (e) => {
   e.preventDefault();
-
-  let arr = Array.from(e.target.parentElement.children);
+  console.log(e.target);
 });
 
 function createBoard(board, arr) {
   for (let i = 0; i < 100; i++) {
     const square = document.createElement('div');
     square.id = i;
-    square.innerHTML = '';
+    square.innerHTML = 'O';
     square.classList.add('square');
     arr.push(square.innerHTML);
     board.appendChild(square);
@@ -71,6 +63,134 @@ function rotatePlayerShips() {
         item.style.transform = 'none';
       }
     });
+  });
+}
+
+let userBoardArr = Array.from(document.querySelectorAll('.square'));
+// let length = 5;
+
+const shipsLength = {
+  destroyer: 5,
+  cruiser: 4,
+  battleship: 3,
+  submarine: 2,
+  carrier: 1,
+};
+
+for (let k in shipsLength) {
+  console.log(shipsLength[k]);
+}
+
+userBoardArr.forEach((item) => {
+  item.addEventListener('mouseenter', (e) => {
+    let target = e.target;
+    let length = playerShips.destroyer.length;
+    mouseIn(target, length);
+  });
+
+  item.addEventListener('mouseleave', () => {
+    console.log('leave');
+    let length = playerShips.destroyer.length;
+    mouseLeave(item, length);
+  });
+
+  item.addEventListener('click', (e) => {
+    let target = e.target;
+    let length = playerShips.destroyer.length;
+    paintBoard(target, length);
+    console.log(playerArr);
+  });
+});
+
+function mouseLeave(target, length) {
+  let targetID = target.id;
+
+  playerArr.forEach((item, index) => {
+    if (index == targetID) {
+      target.classList.add('mouse-leave');
+      target.classList.remove('mouse-in');
+
+      let numOfSquare = 10 * length - 10;
+
+      for (let i = index; i <= numOfSquare + index; i += 10) {
+        // Ship doesn't fit the board
+        if (numOfSquare + index > userBoardArr.length - 1) {
+          console.log('try again');
+        }
+
+        for (let j = 0; j < userBoardArr.length; j++) {
+          // Update board in UI
+          if (i == userBoardArr[j].id) {
+            userBoardArr[j].classList.add('mouse-leave');
+            userBoardArr[j].classList.remove('mouse-in');
+          }
+        }
+      }
+    }
+  });
+}
+
+function paintBoard(target, length) {
+  let targetID = target.id;
+
+  // Loop through player array and mark the square with X
+  playerArr.forEach((item, index, arr) => {
+    if (index == targetID) {
+      item = 'X';
+      target.classList.add('click');
+      target.classList.remove('mouse-in');
+      let numOfSquare = 10 * length - 10;
+
+      for (let i = index; i <= numOfSquare + index; i += 10) {
+        // Ship doesn't fit the board
+        if (numOfSquare + index > userBoardArr.length - 1) {
+          console.log('try again');
+        } else {
+          arr[i] = 'X';
+
+          for (let j = 0; j < userBoardArr.length; j++) {
+            // Update board in UI
+            if (i == userBoardArr[j].id) {
+              // if (userBoardArr[j].classList.contains('occupied')) {
+              //   console.log('taken');
+              //   return;
+              // } else {
+              userBoardArr[j].classList.add('click');
+              userBoardArr[j].classList.remove('mouse-in');
+              userBoardArr[j].classList.add('occupied');
+              // }
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
+function mouseIn(target, length) {
+  let targetID = target.id;
+
+  playerArr.forEach((item, index) => {
+    if (index == targetID) {
+      target.classList.add('mouse-in');
+      target.classList.remove('mouse-leave');
+      let numOfSquare = 10 * length - 10;
+
+      for (let i = index; i <= numOfSquare + index; i += 10) {
+        // Ship doesn't fit the board
+        if (numOfSquare + index > userBoardArr.length - 1) {
+          console.log('try again');
+        }
+
+        for (let j = 0; j < userBoardArr.length; j++) {
+          // Update board in UI
+          if (i == userBoardArr[j].id) {
+            userBoardArr[j].classList.add('mouse-in');
+            userBoardArr[j].classList.remove('mouse-leave');
+          }
+        }
+      }
+    }
   });
 }
 
